d7fe982d907b34ab3085227c995e81e7d333d125
Jongmin Sung <jongmin.sung@gmail.com>
2019-05-31 11:43:00 -0700

    setup conda env

--- campsites.py
+++ campsites.py
@@ -4,7 +4,8 @@ import copy
 import requests
 
 import urllib
-from urlparse import parse_qs
+#from urlparse import parse_qs
+from urllib.parse import parse_qs
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 
@@ -125,9 +126,9 @@ if __name__ == "__main__":
     sites = findCampSites(arg_dict)
     if sites:
         for site in sites:
-            print site + \
+            print (site + \
                 "&arrivalDate={}&departureDate={}" \
                 .format(
                         urllib.quote_plus(formatDate(arg_dict['start_date'])),
-                        urllib.quote_plus(formatDate(arg_dict['end_date'])))
+                        urllib.quote_plus(formatDate(arg_dict['end_date']))))
 
--- campsites.sh
+++ campsites.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-env/bin/python campsites.py --start_date $1 --end_date $2 > result_$1_$2.txt
+#env/bin/python campsites.py --start_date $1 --end_date $2 > result_$1_$2.txt
+python campsites.py --start_date $1 --end_date $2 > result_$1_$2.txt
 RESULTS=`grep -E '^.*[^\n]+.*$' results/result_$1_$2.txt | wc -l`
 if [ $RESULTS -gt "0" ];
 then
